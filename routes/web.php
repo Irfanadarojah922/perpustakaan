2fc9684df92e3b3a2b8723b923c6fe6df508e88e
unknown <yogirahmansetia@gmail.com>
2025-06-29 23:36:08 +0700

    core:add web middleware to all route

--- routes/web.php
+++ routes/web.php
@@ -12,49 +12,52 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});       
+});
 
 // Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 // Route::post('/login', [LoginController::class, 'login']);
 // Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+Route::middleware(['web'])
+    ->group(function () {
+        Route::resource('/register', RegisterController::class)->only(['index']);
 
-Route::resource('/register', RegisterController::class)->only(['index']);
+        Route::get('/dashboard', [DashboardController::class, 'index']);
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
-
-Route::resource('/katalog', KatalogController::class)->only(["index", "show"]);
-
-
-Route::resource('/keanggotaan', AnggotaController::class);
-Route::get('/keanggotaan/{id}/edit', [AnggotaController::class, 'edit']);
-Route::get('/keanggotaan/create', [AnggotaController::class, 'create']);
-Route::get('/keanggotaan/show/{id}', [AnggotaController::class, 'show'])->name('keanggotaan.show');
-Route::delete('/keanggotaan/{id}', [AnggotaController::class, 'destroy']);
-
-Route::put('/keanggotaan/{id}/verifikasi', [AnggotaController::class, 'verifikasi'])->name('keanggotaan.verifikasi');
-
-Route::resource('/sirkulasi/peminjaman', PeminjamanController::class);
-Route::get('/peminjaman/{id}/edit', [PeminjamanController::class, 'edit']);
-Route::get('/peminjaman/add', [PeminjamanController::class, 'add']);
-Route::put('/peminjaman/{id}', [PeminjamanController::class, 'update'])->name('pinjam.update');
-Route::delete('/peminjaman/{id}', [PeminjamanController::class, 'destroy']);
+        Route::resource('/katalog', KatalogController::class)->only(["index", "show"]);
 
 
-Route::resource('/sirkulasi/pengembalian', PengembalianController::class);
-Route::get('/pengembalian/{id}/edit', [PengembalianController::class, 'edit']);
-Route::get('/pengembalian/add', [PengembalianController::class, 'add']);
-Route::put('/pengembalian/{id}', [PengembalianController::class, 'update'])->name('kembali.update');
-Route::delete('/pengembalian/{id}', [PengembalianController::class, 'destroy']);
+        Route::resource('/keanggotaan', AnggotaController::class);
+        Route::get('/keanggotaan/{id}/edit', [AnggotaController::class, 'edit']);
+        Route::get('/keanggotaan/create', [AnggotaController::class, 'create']);
+        Route::get('/keanggotaan/show/{id}', [AnggotaController::class, 'show'])->name('keanggotaan.show');
+        Route::delete('/keanggotaan/{id}', [AnggotaController::class, 'destroy']);
+
+        Route::put('/keanggotaan/{id}/verifikasi', [AnggotaController::class, 'verifikasi'])->name('keanggotaan.verifikasi');
+
+        Route::resource('/sirkulasi/peminjaman', PeminjamanController::class);
+        Route::get('/peminjaman/{id}/edit', [PeminjamanController::class, 'edit']);
+        Route::get('/peminjaman/add', [PeminjamanController::class, 'add']);
+        Route::put('/peminjaman/{id}', [PeminjamanController::class, 'update'])->name('pinjam.update');
+        Route::delete('/peminjaman/{id}', [PeminjamanController::class, 'destroy']);
 
 
-Route::resource('buku', BookController::class)->only(["store", "create"]);
+        Route::resource('/sirkulasi/pengembalian', PengembalianController::class);
+        Route::get('/pengembalian/{id}/edit', [PengembalianController::class, 'edit']);
+        Route::get('/pengembalian/add', [PengembalianController::class, 'add']);
+        Route::put('/pengembalian/{id}', [PengembalianController::class, 'update'])->name('kembali.update');
+        Route::delete('/pengembalian/{id}', [PengembalianController::class, 'destroy']);
 
-Route::get('search/peminjaman/anggota', [PeminjamanController::class, 'searchAnggotaByNIK'])->name('search.peminjaman.anggota');
-Route::get('search/peminjaman/buku', [PeminjamanController::class, 'searchBukuByKodeBuku'])->name('search.peminjaman.buku');
 
-Route::get('search/pengembalian/buku', [PengembalianController::class, 'searchBukuByKodeBuku'])->name('search.pengembalian.buku');
+        Route::resource('buku', BookController::class)->only(["store", "create"]);
 
-Route::get('search/keanggotaan/anggota', [AnggotaController::class, 'searchAnggota'])->name('search.keanggotaan.anggota');
+        Route::get('search/peminjaman/anggota', [PeminjamanController::class, 'searchAnggotaByNIK'])->name('search.peminjaman.anggota');
+        Route::get('search/peminjaman/buku', [PeminjamanController::class, 'searchBukuByKodeBuku'])->name('search.peminjaman.buku');
 
-Route::get('search/buku/dipinjam', [PengembalianController::class, 'searchBukuByKodeBukuInBorrowed'])->name('search.buku.dipinjam');
+        Route::get('search/pengembalian/buku', [PengembalianController::class, 'searchBukuByKodeBuku'])->name('search.pengembalian.buku');
+
+        Route::get('search/keanggotaan/anggota', [AnggotaController::class, 'searchAnggota'])->name('search.keanggotaan.anggota');
+
+        Route::get('search/buku/dipinjam', [PengembalianController::class, 'searchBukuByKodeBukuInBorrowed'])->name('search.buku.dipinjam');
+
+    });
