acc679e35d15c93bb69dd34ca36ad897c22cd53f
unknown <yogirahmansetia@gmail.com>
2025-07-01 12:17:14 +0700

    fix:register url

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/register', RegisterController::class)->only(['index']);
 
 // Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 // Route::post('/login', [LoginController::class, 'login']);
@@ -20,7 +21,6 @@ Route::get('/', function () {
 
 Route::middleware(['web', 'auth'])
     ->group(function () {
-        Route::resource('/register', RegisterController::class)->only(['index']);
 
         Route::get('/dashboard', [DashboardController::class, 'index']);
 
